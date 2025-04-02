fd038dbec2a0f5b33a6388a0e57bc343ea24aaff
yuta-nakajima <yuta-nakajima@NTJ230120-06.local>
2025-04-02 16:22:54 +0900

    holdHighlightを削除

--- nvim/init.lua
+++ nvim/init.lua
@@ -254,7 +254,6 @@ highlight LspReferenceRead  cterm=underline ctermfg=1 ctermbg=8 gui=underline gu
 highlight LspReferenceWrite cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
 augroup lsp_document_highlight
   autocmd!
-  autocmd CursorHold,CursorHoldI * lua vim.lsp.buf.document_highlight()
   autocmd CursorMoved,CursorMovedI * lua vim.lsp.buf.clear_references()
 augroup END
 
